f6b074ca38b1afe62dae3164167104a41671c8d3
andreagosti9 <andrea.agosti9@gmail.com>
2023-06-01 14:36:38 +0200

    added some serious analysis

--- app.r
+++ app.r
@@ -3,33 +3,104 @@
 suppressMessages(suppressWarnings(library("distr")))
 suppressMessages(suppressWarnings(library("stats")))
 
-source("data-read-support.r")
+# Meglio runnare il programma con Rscript e non con R per evitare che ogni volta stampi anche tutto il file in esecuzione.
+
+rawData <- "revenues2022.txt"
+
+df <- read.table(
+        rawData, 
+        header = FALSE, 
+        sep = c("\t"), 
+        colClasses = c("NULL", rep("numeric", 16)), 
+        col.names = 0:16, 
+        numerals = "no.loss")
+
+# Il numero di indice del vettore matcha con il nome della colonna, oltre che essere scritto (giusto per avere un riferimento nel codice).
+columnsMap <- c(
+        "(1) Settimana di riferimento",
+        "(2) VENDITA: Trekking",
+        "(3) VEDNITA: MTB",
+        "(4) VENDITA: Strada",
+        "(5) VENDITA: Abbigliamento",
+        "(6) VENDITA: Accessori",
+        "(7) VENDITA: Componenti",
+        "(8) VENDITA: Componenti venduti tramite SAM",
+        "(9) VENDITA: SAM",
+        "(10) RICAVO: Trekking",
+        "(11) RICAVO: MTB",
+        "(12) RICAVO: Strada",
+        "(13) RICAVO: Abbigliamento",
+        "(14) RICAVO: Accessori",
+        "(15) RICAVO: (Totale) Componenti venduti standalone e venduti tramite SAM",
+        "(16) RICAVO: SAM"
+)
+
+
+# Utilities
+prec <- function(x){ format(round(x, 4), nsmall = 4) }
 
 
 # Analisi
-df <- readData()
-
 weeks <- df[,1]
-components.sold.through.assistance.norm <- df[,8] / sum(df[,8])
-assistance.services.performed.norm <- df[,9] / sum(df[,9])
+components.sold.standalone <- df[,7]
+components.sold.through.assistance.services <- df[,8]
+assistance.services.performed <- df[,9]
+total.components.income <- df[,15]
+total.assistance.services.income <- df[,15]
+
+# Si osserva se ci sono relazioni (si) e di che tipo fra assistenze eseguite e componenti venduti tramite di esse
+rxy <- cor(
+        components.sold.through.assistance.services, 
+        assistance.services.performed)
 
 plot(
-        weeks, 
-        components.sold.through.assistance.norm - assistance.services.performed.norm, 
-        col = "red", 
-        type = "l", 
-        ylim = c(-0.03, 0.03))
-lines(
-        weeks, 
-        rep(0, 52),
-        col = "gray")
-# lines(
-#         weeks, 
-#         assistance.services.performed.norm,
-#         col = "blue")
+        assistance.services.performed,
+        components.sold.through.assistance.services,
+        type = "p",
+        col = "blue")
 
-# legend(
-#         "topleft",
-#         legend = c("Componenti venduti tramite assistenza", "Servizi assistenza effettuati"),
-#         pch = 15,
-#         col = c("red", "blue"))
+assistance.related.components <- lm(components.sold.through.assistance.services ~ assistance.services.performed)
+
+mtext("", line = 0)
+mtext(
+        paste("Corr. coef. : ", prec(rxy)),
+        line = 1)
+mtext(
+        paste("B : ", prec(assistance.related.components$coefficients[2])),
+        line = 2)
+
+
+# Si prova anche ad osservare se assistenze eseguite e componenti venduti a parte sono in relazione (i clienti potrebbero essere incentivati, dopo una riparazione ad acquistare un ulteriore ricambio di scorta)
+rxy <- cor(
+        components.sold.standalone, 
+        assistance.services.performed)
+
+plot(
+        assistance.services.performed,
+        components.sold.standalone,
+        type = "p",
+        col = "blue")
+
+standalone.components <- lm(components.sold.standalone ~ assistance.services.performed)
+
+mtext("", line = 0)
+mtext(
+        paste("Corr. coef. : ", prec(rxy)),
+        line = 1)
+
+
+# Proviamo a sovrapporli e vederne l'andamento nelle varie settimane
+plot(
+        weeks,
+        components.sold.through.assistance.services,
+        type = "l",
+        col = "red",
+        ylim = c(0, 150))
+lines(
+        weeks,
+        components.sold.standalone,
+        col = "green")
+lines(
+        weeks,
+        assistance.services.performed,
+        col = "grey")
