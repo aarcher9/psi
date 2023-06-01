8ed25a0f14bdd0e921d5defcd2dc3312030e11dc
andreagosti9 <andrea.agosti9@gmail.com>
2023-06-01 10:56:18 +0200

    added analysis
    sold components through assistance and number of assistance services performed

--- app.r
+++ app.r
@@ -9,10 +9,27 @@ source("data-read-support.r")
 # Analisi
 df <- readData()
 
-componentsOnlySells <- df[,7]
-componentsOnlyAndSMASells <- df[,8]
-componentsSMASells <- componentsOnlyAndSMASells - componentsOnlySells
+weeks <- df[,1]
+components.sold.through.assistance.norm <- df[,8] / sum(df[,8])
+assistance.services.performed.norm <- df[,9] / sum(df[,9])
 
-print(mean(componentsOnlySells))
-print(mean(componentsOnlyAndSMASells))
-print(mean(componentsSMASells))
+plot(
+        weeks, 
+        components.sold.through.assistance.norm - assistance.services.performed.norm, 
+        col = "red", 
+        type = "l", 
+        ylim = c(-0.03, 0.03))
+lines(
+        weeks, 
+        rep(0, 52),
+        col = "gray")
+# lines(
+#         weeks, 
+#         assistance.services.performed.norm,
+#         col = "blue")
+
+# legend(
+#         "topleft",
+#         legend = c("Componenti venduti tramite assistenza", "Servizi assistenza effettuati"),
+#         pch = 15,
+#         col = c("red", "blue"))
