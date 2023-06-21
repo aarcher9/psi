532201bc521d7b8dd93a4c47d2b4e3859c35084e
andreagosti9 <andrea.agosti9@gmail.com>
2023-06-21 09:06:14 +0200

    better code

--- app.r
+++ app.r
@@ -1,47 +1,19 @@
 suppressMessages(suppressWarnings(library("distr")))
 suppressMessages(suppressWarnings(library("stats")))
 
-# Meglio runnare il programma con Rscript e non con R per evitare che ogni volta stampi anche tutto il file in esecuzione.
-
-rawData <- "revenues2022.txt"
+raw_data <- "revenues2022.txt"
 
 df <- read.table(
-        rawData, 
-        header = FALSE, 
-        sep = c("\t"), 
-        colClasses = c("NULL", rep("numeric", 16)), 
-        col.names = 0:16, 
+        raw_data,
+        header = FALSE,
+        sep = c("\t"),
+        colClasses = c("NULL", rep("numeric", 16)),
+        col.names = 0:16,
         numerals = "no.loss")
 
-# Il numero di indice del vettore matcha con il nome della colonna, oltre che essere scritto (giusto per avere un riferimento nel codice).
-columnsMap <- c(
-        "(1) Settimana di riferimento",
-        "(2) VENDITA: Trekking",
-        "(3) VEDNITA: MTB",
-        "(4) VENDITA: Strada",
-        "(5) VENDITA: Abbigliamento",
-        "(6) VENDITA: Accessori",
-        "(7) VENDITA: Componenti",
-        "(8) VENDITA: Componenti venduti tramite SAM",
-        "(9) VENDITA: SAM",
-        "(10) RICAVO: Trekking",
-        "(11) RICAVO: MTB",
-        "(12) RICAVO: Strada",
-        "(13) RICAVO: Abbigliamento",
-        "(14) RICAVO: Accessori",
-        "(15) RICAVO: (Totale) Componenti venduti standalone e venduti tramite SAM",
-        "(16) RICAVO: SAM"
-)
 
-
-# Utilities
-prec <- function(x){ format(round(x, 4), nsmall = 4) }
-
-
-
-# Analisi
-weeks <- df[,1]
-sale <- list(
+weeks <- df[, 1]
+sales <- list(
         bikes = list(
                 trekking = df[, 2],
                 mtb = df[, 3],
@@ -49,14 +21,37 @@ sale <- list(
         ),
         components = list(
                 alone = df[, 7],
-                through.assistance = df[, 8]
+                through.assistances = df[, 8]
         ),
-        assistance.performed = df[, 9]
+        assistances = df[, 9]
 )
 
-income <- list(
+incomes <- list(
         components = list(
                 total = df[, 15]
         ),
-        assistance = df[, 16]
+        assistances = df[, 16]
 )
+
+
+# Utilities
+prec <- function(x) {
+        format(round(x, 4), nsmall = 4)
+}
+
+
+# Analisi
+"Vendite soli componenti"
+mean(sales$components$alone)
+
+"Vendite componenti tramite assistenza"
+mean(sales$components$through.assistances)
+
+"Assistenze effettuate"
+mean(sales$assistances)
+
+"Ricavi vendite componenti (cumulativi)"
+mean(incomes$components$total)
+
+"Ricavi servizio di assistenza"
+mean(incomes$assistances)
